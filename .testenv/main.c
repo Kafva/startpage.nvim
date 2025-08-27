5af8bbfd4ad5e80ef32d33f2841018f580894567
Kafva <kafva.one@gmail.com>
2025-08-27 05:58:15 +0200

    Add testdata

---
+++ .testenv/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <sys/socket.h>
+#include <net/if.h>
+#include <arpa/inet.h>
+
+int main(int argc, char *argv[], char *envp[]) {
+
+    printf("xD\n");
+
+    return 0;
+}
